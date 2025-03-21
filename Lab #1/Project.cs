f0d466a37ba76405eea55efe3c487ed52d786913
NinjaP15 <ninjabomber56@gmail.com>
2025-03-21 17:58:01 +0000

    using System;
    class Program
    {
        static void Main(string[] args)
        {
            /*
         Name: Silas Kean
            Title: IT-1050-Lab #1
            */
    
            // Step 1
            Console.WriteLine("Silas Kean - IT-1050-Lab #1");
    
            // Step 3
            int favoriteNumber = 15;
            string favoriteLanguage = "Python";
            double programsWritten = 2;
            bool hasExperience = true;
    
            Console.WriteLine("Favorite Number: " + favoriteNumber);
            Console.WriteLine("Favorite Language: " + favoriteLanguage);
            Console.WriteLine("Programs Written: " + programsWritten);
            Console.WriteLine("Programming Experience: " + hasExperience);
    
            // Step 5
            const string schoolName = "Cuyahoga Community College";
            Console.WriteLine("School Name: " + schoolName);
        }
    }

---
+++ Lab #1/Project.cs
@@ -0,0 +1,30 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+     Name: Silas Kean
+        Title: IT-1050-Lab #1
+        */
+
+        // Step 1
+        Console.WriteLine("Silas Kean - IT-1050-Lab #1");
+
+        // Step 3
+        int favoriteNumber = 15;
+        string favoriteLanguage = "Python";
+        double programsWritten = 2;
+        bool hasExperience = true;
+
+        Console.WriteLine("Favorite Number: " + favoriteNumber);
+        Console.WriteLine("Favorite Language: " + favoriteLanguage);
+        Console.WriteLine("Programs Written: " + programsWritten);
+        Console.WriteLine("Programming Experience: " + hasExperience);
+
+        // Step 5
+        const string schoolName = "Cuyahoga Community College";
+        Console.WriteLine("School Name: " + schoolName);
+    }
+}
