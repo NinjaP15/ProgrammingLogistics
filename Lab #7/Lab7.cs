ce000e5d7bc70d74ab6b143e55b0d1597b5c4a17
NinjaP15 <ninjabomber56@gmail.com>
2025-04-30 03:33:21 +0000

    using System;
    class Book
    {
        private string title;
        private string author;
    
        // Default Constructor
        public Book()
        {
            title = "Unknown Title";
            author = "Unknown Author";
        }
    
        // Constructor with title parameter
        public Book(string title)
        {
            this.title = title;
            author = "Unknown Author";
        }
    
        // Constructor with title and author parameters
        public Book(string title, string author)
        {
            this.title = title;
            this.author = author;
        }
    
        // Getters
        public string GetTitle()
        {
            return title;
        }
    
        public string GetAuthor()
        {
            return author;
        }
    
        // Setters
        public void SetTitle(string newTitle)
        {
            title = newTitle;
        }
    
        public void SetAuthor(string newAuthor)
        {
            author = newAuthor;
        }
    }
    
    class Program
    {
        static void Main(string[] args)
        {
            // Problem 1: Default constructor
            Book book1 = new Book();
            Console.WriteLine(book1.GetTitle());
            book1.SetTitle("C# Fundamentals");
            Console.WriteLine(book1.GetTitle());
    
            // Problem 2: Parameterized constructor (title)
            Book book2 = new Book("Advanced C#");
            Console.WriteLine(book2.GetTitle());
    
            // Problem 3: Constructor overloading (title + author)
            Book book3 = new Book("Advanced C#", "John Doe");
            Console.WriteLine("Title: " + book3.GetTitle());
            Console.WriteLine("Author: " + book3.GetAuthor());
    
            // Problem 4: Update fields using setter methods
            Book book4 = new Book();
            book4.SetTitle("Mastering C#");
            book4.SetAuthor("Jane Smith");
            Console.WriteLine("Title: " + book4.GetTitle());
            Console.WriteLine("Author: " + book4.GetAuthor());
        }
    }

--- Lab #7/Lab7.cs
+++ Lab #7/Lab7.cs
@@ -16,7 +16,7 @@ class Book
     public Book(string title)
     {
         this.title = title;
-        author = "Unknown Author"; // Default value
+        author = "Unknown Author";
     }
 
     // Constructor with title and author parameters
@@ -55,13 +55,13 @@ class Program
     {
         // Problem 1: Default constructor
         Book book1 = new Book();
-        Console.WriteLine(book1.GetTitle()); // Output: Unknown Title
+        Console.WriteLine(book1.GetTitle()); 
         book1.SetTitle("C# Fundamentals");
-        Console.WriteLine(book1.GetTitle()); // Output: C# Fundamentals
+        Console.WriteLine(book1.GetTitle()); 
 
         // Problem 2: Parameterized constructor (title)
         Book book2 = new Book("Advanced C#");
-        Console.WriteLine(book2.GetTitle()); // Output: Advanced C#
+        Console.WriteLine(book2.GetTitle()); 
 
         // Problem 3: Constructor overloading (title + author)
         Book book3 = new Book("Advanced C#", "John Doe");
@@ -69,7 +69,7 @@ class Program
         Console.WriteLine("Author: " + book3.GetAuthor());
 
         // Problem 4: Update fields using setter methods
-        Book book4 = new Book(); // Can be any constructor
+        Book book4 = new Book();
         book4.SetTitle("Mastering C#");
         book4.SetAuthor("Jane Smith");
         Console.WriteLine("Title: " + book4.GetTitle());
